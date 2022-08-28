5261885898ec3f937dabecbe81d9a769900f951d
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2022-08-28 15:24:59 -0300

    Update relatorio.py

--- report_using_Salt/relatorio.py
+++ report_using_Salt/relatorio.py
@@ -1,92 +1,117 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
 
 import json
 import sys
 import os
 
-arg_len = len(sys.argv)-1
+class Report:
+          
+    def __init__(self): 
 
-if not ((arg_len > 1) and (arg_len <= 2)):
-    print("Erro, número de argumentos inválido")
-    sys.exit(10)
+        if not ((len(sys.argv)-1 > 1) and (len(sys.argv)-1 <= 2)):
+            print("Erro, número de argumentos inválido")
+            sys.exit(9)
 
-# Joga os argumentos para variáveis:
-sector_arg1 = sys.argv[1]
-machinetype_arg2 = sys.argv[2]
+        # As veriaveis abaixo herdam os argumentos:
+        self.sector_arg1 = sys.argv[1]
+        self.machinetype_arg2 = sys.argv[2]
 
-# Tratativa de erro para caso do tipo de maquina não existir:
-if not ((machinetype_arg2 == 'desktops') or (machinetype_arg2 == 'laptops')):
-    print('Machine type', machinetype_arg2, 'not found!')
-    sys.exit(10)
+        # Tratativa de erro para quando o tipo de maquina digitada não existir (desktop ou laptop):
+        if not ((self.machinetype_arg2 == 'desktops') or (self.machinetype_arg2 == 'laptops')):
+            print('Machine type', self.machinetype_arg2, 'not found!')
+            sys.exit(10)
 
-# Tratativa de erro para caso do setor não existir:
-stream_groups = os.popen('ls /var/log/salt-report/'+machinetype_arg2)
-output_groups = stream_groups.readlines()
+        # Tratativa de erro para quando o setor digitado não existir:
+        stream_sector = os.popen('ls /var/log/salt-report/'+self.machinetype_arg2)
+        sector = stream_sector.readlines()
 
-if sector_arg1+'\n' not in output_groups:
-    print('Sector', sector_arg1, 'not found!')
-    sys.exit(10)
+        if self.sector_arg1+'\n' not in sector:
+            print('Sector', self.sector_arg1, 'not found!')
+            sys.exit(11)
 
-# Pega os arquivos dentro de report de cada maquina:
-stream = os.popen('ls /var/log/salt-report/'+machinetype_arg2+'/'+sector_arg1+'/*.log 2>/dev/null')
-output = stream.readlines()
+        # Pega os arquivos dentro de report de cada setor:
+        stream_files = os.popen('ls /var/log/salt-report/'+self.machinetype_arg2+'/'+self.sector_arg1+'/*.log 2>/dev/null')
+        report_files = stream_files.readlines()
 
-if not output:
-    print('No files were found!')
-    sys.exit(10)
+        if not report_files:
+            print('No files were found!')
+            sys.exit(12)
 
-def collect(report):
-    updates = ''
-    categorias = ''
-    title = ''
+        self.uptodate = []
+        self.outofdate = []
 
-    message = """\n######################\n# System Information #\n######################\n"""
+        self.find_files(report_files)
 
 
-    lines_system = """Name: {0}\nMachine type: {1}\nSector: {2}\nBios Detail: {3}\nOS Version: {4}\nCollection Date: {5}\nInstall Date: {6}\nUsers: {7}"""
+    def find_files(self, report_files):
+        # Mostra quantos arquivos de log foram encontrados e quem são:
+        if len(report_files) != 0:
+            message_total_files = """\n######################\n# Total files found  #\n######################\n"""
+            print(message_total_files + '\n' + 'For sector', self.sector_arg1, 'We have a total of:', len(report_files), 'file(s)')
 
-    nome = str(report['SystemInfo']['name'])
-    machinetype = str(report['SystemInfo']['MachineType'])
-    sector = str(report['SystemInfo']['sector'])
-    bios_detail = str(report['SystemInfo']['bios_details'])
-    os_version = str(report['SystemInfo']['os_version'])
-    collection_date = str(report['SystemInfo']['collection_date'])
-    install_date = str(report['SystemInfo']['install_date'])
-    users = str(report['SystemInfo']['users'])
+            message_hosts_found = """\n########################\n# The Hosts found are  #\n########################\n"""
+            hosts = ''
 
-    print(message + '\n' + lines_system.format(nome, machinetype, sector, bios_detail, os_version, collection_date, install_date, users))
-
-    if report['Updates'] != 'Nothing to return':
-        
-        for chave in report['Updates'].keys():
-            updates += str(report['Updates'][chave]['KBs']) + '\n'
-            title += str(report['Updates'][chave]['Title']) + '\n'
-            categorias += str(report['Updates'][chave]['Categories']) + '\n'
-
-        message = """\n######################\n# Update Information #\n######################\n"""
-
-        lines_update = """Updates available: \n{0}\nCategory: \n{1}\nUpdate Title: \n{2}"""
-
-        print(message + '\n' + lines_update.format(updates, categorias, title))
-
-    else:
-
-        message = """\n######################\n# Update Information #\n######################\n"""
-        print(message + '\n' + 'Windows updated!')
-
-for fileslog in output:
+            for fileshost in report_files:
     
-    # Removendo '\n' do nome do arquivo:
-    fileslog = fileslog.strip()
+                # Removendo '\n' do nome do arquivo:
+                fileshost = fileshost.strip()
+                fileshost = fileshost.split('/')
+                hosts += fileshost[len(fileshost)-1][0:7] + '\n'
+            print(message_hosts_found + '\n' + 'For sector', self.sector_arg1, 'We found the Hosts below:' + '\n' + hosts)
 
-    # Abrindo o arquivo JSON:
-    with open(fileslog, 'r') as f:
-        # Retorna o JSON como um dicionário Python:
-        reportJson = json.load(f)
+            self.build(report_files)
 
-    collect(reportJson)
 
-    print('_________________________________________________________________________')
+    def find_update(self, report):
 
-    # Fechando o arquivo aberto:
-    f.close()
+        nome_host = str(report['SystemInfo']['name'])
+
+        if report['Updates'] != 'Nothing to return':
+            self.outofdate.append(nome_host)
+        
+        else:
+            self.uptodate.append(nome_host)
+
+
+    def build(self, report_files):
+     
+        for fileslog in report_files:
+            # Removendo '\n' no nome do arquivo:
+            fileslog = fileslog.strip()
+    
+            # Abrindo o arquivo JSON:
+            with open(fileslog, 'r') as f:
+                # Retorna o JSON como um dicionário Python:
+                reportJson = json.load(f)
+    
+            self.find_update(reportJson)
+    
+            # Fechando o arquivo aberto:
+            f.close() 
+
+    def print_report(self):
+        update = ''
+        print()
+
+        if len(self.outofdate) != 0:
+            print(len(self.uptodate), 'hosts are up-to-date.')
+            print(len(self.outofdate), 'hosts need update. Please do it!')
+        
+            message_outofdate = """\n######################\n# Hosts Out of Date  #\n######################\n"""
+
+            for out_of_date_hosts in self.outofdate:
+                # Removendo '\n' no nome do arquivo:
+                update += out_of_date_hosts + '\n'
+
+            print(message_outofdate, '\n'+ update)
+        else:
+            print('All hosts are up-to-date!')
+
+
+if __name__ == "__main__":
+
+        init_class = Report()
+        init_class.print_report()
