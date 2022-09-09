9f7b11b17a11bc475187cdf20845f907d21d99e9
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2022-09-09 09:45:32 -0300

    Update relatorio.py

--- report_using_Salt/relatorio.py
+++ report_using_Salt/relatorio.py
@@ -10,8 +10,21 @@ class Report:
           
     def __init__(self): 
 
+        try:
+            if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+                self.help()
+                sys.exit(0)
+
+            elif sys.argv[1] == 'search':
+                self.search_minion()
+                sys.exit(0)
+        
+        except IndexError:
+            pass
+
         if not ((len(sys.argv)-1 > 1) and (len(sys.argv)-1 <= 2)):
-            print("Erro, número de argumentos inválido")
+            print("Error, invalid number of arguments\n")
+            self.help()
             sys.exit(9)
 
         # As veriaveis abaixo herdam os argumentos:
@@ -23,17 +36,16 @@ class Report:
             print('Machine type', self.machinetype_arg2, 'not found!')
             sys.exit(10)
 
-        # Tratativa de erro para quando o setor digitado não existir:
-        stream_sector = os.popen('ls /var/log/salt-report/'+self.machinetype_arg2)
-        sector = stream_sector.readlines()
+        path_machinetype = '/var/log/salt-report/'+self.machinetype_arg2
+        sector = os.listdir(path_machinetype)
 
-        if self.sector_arg1+'\n' not in sector:
+        if self.sector_arg1 not in sector:
             print('Sector', self.sector_arg1, 'not found!')
             sys.exit(11)
 
         # Pega os arquivos dentro de report de cada setor:
-        stream_files = os.popen('ls /var/log/salt-report/'+self.machinetype_arg2+'/'+self.sector_arg1+'/*.log 2>/dev/null')
-        report_files = stream_files.readlines()
+        self.path_report = '/var/log/salt-report/'+self.machinetype_arg2+'/'+self.sector_arg1+'/'
+        report_files = os.listdir(self.path_report)
 
         if not report_files:
             print('No files were found!')
@@ -45,6 +57,9 @@ class Report:
         self.find_files(report_files)
 
 
+    def help(self):
+        print("Use:\nrelatorio <Sector> <Machine type>\n\nCreate a report using SaltStack and log generated using Shell Script\n\nOptions:\n   Sector - can be sector in the company\n   Machine type - can be 'Desktops' or 'Laptops' (without quotes)\n\nIf you want to use the 'search' option, see below:\n   search <serial>\n\nYou can also use like this:\n   search <serial1> <serial2>...")
+
     def find_files(self, report_files):
         # Mostra quantos arquivos de log foram encontrados e quem são:
         if len(report_files) != 0:
@@ -55,20 +70,81 @@ class Report:
             hosts = ''
 
             for fileshost in report_files:
-    
-                # Removendo '\n' do nome do arquivo:
-                fileshost = fileshost.strip()
-                fileshost = fileshost.split('/')
-                hosts += fileshost[len(fileshost)-1][0:7] + '\n'
-            print(message_hosts_found + '\n' + 'For sector', self.sector_arg1, 'We found the Hosts below:' + '\n' + hosts)
+                hosts += fileshost[0:7] + '\n'
 
+            print(message_hosts_found + '\n' + 'For sector', self.sector_arg1, 'We found the Hosts below:' + '\n' + hosts)
             self.build(report_files)
+
+
+    def search_minion(self):
+
+        if not (len(sys.argv)-1 > 1):
+            print("Error, invalid number of arguments in search\n")
+            self.help()
+            sys.exit(9)
+
+        minions = sys.argv[2:]
+        found_files_by_search = []
+        not_found_files_by_search = []
+
+        for find in minions:
+            find2 = find
+            find = find + '.log'
+
+            host_find = self.search_for_files(find, "/var/log/salt-report/")
+
+            if host_find:
+                found_files_by_search.append(host_find)
+            else:
+                not_found_files_by_search.append(find2)
+
+        if len(found_files_by_search) == 0 and len(not_found_files_by_search) >= 0:
+            print("\nNo serial found!")
+            sys.exit(1)
+        elif len(found_files_by_search) >= 1 and len(not_found_files_by_search) >= 1:
+            print("\nSome serials could not be found!\nThe serial(s) is/are:\n", not_found_files_by_search)
+
+        #print('found_files_by_search:\n', found_files_by_search)
+
+        for found in found_files_by_search:
+            with open(found, 'r') as f:
+                # Retorna o JSON como um dicionário Python:
+                reportJson = json.load(f)
+    
+            self.find_info(reportJson)
+    
+            # Fechando o arquivo aberto:
+            f.close() 
+
+
+    def find_info(self, report):
+
+        host_serial = str(report['SystemInfo']['hardware_serial'])
+        host_name = str(report['SystemInfo']['name'])
+        host_sector = str(report['SystemInfo']['sector'])
+
+        host_os_version = str(report['SystemInfo']['os_version'])
+        host_MachineType = str(report['SystemInfo']['MachineType'])
+        host_users = str(report['SystemInfo']['users'])
+
+        print('\n\n######################\n# ', host_serial,'          #\n######################\n')
+
+        print('Serial: {0}\nName: {1}\nSector: {2}\nOS_Version: {3}\nMachine type: {4}\nUsers: {5}'.format(
+        host_serial, host_name, host_sector, host_os_version, host_MachineType, host_users))
+
+    def search_for_files(self, filename, search_path):
+        result = ''
+
+        # Walking top-down from the /var/log/salt-report/
+        for root, dir, files in os.walk(search_path):
+            if filename in files:
+                result = (os.path.join(root, filename))
+        return result
 
 
     def find_update(self, report):
 
-        nome_host = str(report['SystemInfo']['name'])
-
+        nome_host = str(report['SystemInfo']['hardware_serial'])
         if report['Updates'] != 'Nothing to return':
             self.outofdate.append(nome_host)
         
@@ -79,11 +155,10 @@ class Report:
     def build(self, report_files):
      
         for fileslog in report_files:
-            # Removendo '\n' no nome do arquivo:
-            fileslog = fileslog.strip()
-    
+
             # Abrindo o arquivo JSON:
-            with open(fileslog, 'r') as f:
+            file_log = self.path_report + fileslog
+            with open(file_log, 'r') as f:
                 # Retorna o JSON como um dicionário Python:
                 reportJson = json.load(f)
     
